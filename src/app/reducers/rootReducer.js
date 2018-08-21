3e1a5cd90f49c85aff71bd27ada58ba2797d4195
Brian Fan <kuomu.fan@yahoo.com>
2018-08-21 14:30:08 -0700

    set config for redux store

---
+++ src/app/reducers/rootReducer.js
@@ -0,0 +1,7 @@
+import { combineRecers } from "redux";
+
+const rootReducer = combineRecers({
+
+})
+
+export default rootReducer;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "redux";
 import { BrowserRouter } from "react-router-dom";
 import "semantic-ui-css/semantic.min.css";
 import "./index.css";
 import App from "./app/layout/App";
 import registerServiceWorker from "./registerServiceWorker";
+import { configureStore } from './app/store/configureStore'
+
+const store = configureStore();
 
 const rootEl = document.getElementById("root");
 
 let render = () => {
   ReactDOM.render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
     rootEl
   );
 };
